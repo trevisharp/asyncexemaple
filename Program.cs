29f93a6b1412832fe266d88b69ac7a839524a9d3
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2022-04-18 13:44:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
 ﻿using System;
+using System.Threading.Tasks;
 
-Console.WriteLine("Hello World!");
+Action evento = () =>
+{
+    Console.WriteLine("Xispita!!");
+};
+
+Action outroevento = async () =>
+{
+    int result = await DemoradaAsync();
+    Console.WriteLine(result);
+};
+
+while (true)
+{
+    string value = Console.ReadLine();
+    if (value == "xispita")
+        evento();
+    else if (value == "demora")
+        outroevento();
+}
+
+async Task<int> DemoradaAsync()
+{
+    int resultado = await Task<int>
+        .Factory.StartNew(Demorada);
+    return resultado;
+}
+
+int Demorada()
+{
+    int max = 2 * 1000 * 1000 * 1000;
+    for (int i = 0; i < max; i++) ;
+    return 8;
+}
